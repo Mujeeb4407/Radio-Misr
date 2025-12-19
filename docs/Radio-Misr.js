80aee412d152828467bbcee621a8ccaae8f55279
Mujeeb raji <mujeebraji48@gmail.com>
2025-12-19 16:54:36 +0100

    Move site files to docs folder

---
+++ docs/Radio-Misr.js
@@ -0,0 +1,31 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+const faders = document.querySelectorAll('.fade-in');
+const appearOnScroll = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    })
+},
+{
+    threshold: 0.2
+}
+);
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
